6d1816d6b8511d9c608209a040fd3a0405460029
LenaPradoP <lena@atajo.es>
2024-12-02 13:42:25 +0100

    Ejercicio 2

--- 1.2-Nivell1.php
+++ 1.2-Nivell1.php
@@ -16,4 +16,15 @@ echo("$boolean  <br>");
 define("NOMBRE", "Lena Prado");
 echo('<h1>' . NOMBRE . '</h1>');
 
+echo ("<h1> <u> Ejercicio 2 </u> </h1>");
+
+$mensaje1 = "Hello, World!";
+
+echo $mensaje1 . ' ' . $mensaje2 . "<br>";
+echo (strtoupper($mensaje1) . "<br>");
+echo ('El string tiene ' . strlen($mensaje1) . ' caracteres.' . "<br>");
+echo (strrev($mensaje1) . "<br>");
+
+$mensaje2 = "Aquest és el curs de PHP.";
+echo $mensaje1 . ' ' . $mensaje2 . "<br>";
 ?>
