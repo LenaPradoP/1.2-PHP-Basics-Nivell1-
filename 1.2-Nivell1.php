5b4fd15afe0687d7cf28b8a97054ca16078151d8
LenaPradoP <lena@atajo.es>
2024-12-02 12:57:09 +0100

    Ejercicio 1

---
+++ 1.2-Nivell1.php
@@ -0,0 +1,19 @@
+
+<?php
+
+echo ("<h1> <u> Ejercicio 1 </u> </h1>");
+
+$entero = 1;
+$decimal = 1.5;
+$palabra = "Hola";
+$boolean = true;
+
+echo("$entero <br>");
+echo("$decimal <br>");
+echo("$palabra <br>");
+echo("$boolean  <br>"); 
+
+define("NOMBRE", "Lena Prado");
+echo('<h1>' . NOMBRE . '</h1>');
+
+?>
